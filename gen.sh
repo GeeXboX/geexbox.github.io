faf8d9d9617ea7456864acf48e2d3702b6297be6
Alexis Saettler <asbin@asbin.org>
2006-02-08 23:19:56 +0000

    use sablotron to generate pages if it's present

--- gen.sh
+++ gen.sh
@@ -5,8 +5,21 @@ LANGS="en fr de"
 
 for l in $LANGS; do
   mkdir -p $l
-  for p in $PAGES; do
-    xsltproc --stringparam filename $p.html --stringparam lang $l xml/geexbox.xsl xml/$p.xml > $l/$p.html
-  done
-  xsltproc --stringparam date "`date "+%Y-%m-%dT%H:%M:%SZ"`" --stringparam lang $l xml/atom.xsl xml/index.xml > $l/news.xml
+  if [ -n `which sabcmd` ]; then
+
+    pagelist=""
+    for p in $PAGES; do
+      pagelist=$pagelist" xml/$p.xml $l/$p.html"
+    done
+    sabcmd --batch-xsl xml/geexbox.xsl $pagelist \$filename=$p.html \$lang=$l
+    sabcmd --batch-xsl xml/atom.xsl xml/index.xml $l/news.xml \$date=`date "+%Y-%m-%dT%H:%M:%SZ"` \$lang=$l
+
+  else
+
+    for p in $PAGES; do
+      xsltproc --stringparam filename $p.html --stringparam lang $l xml/geexbox.xsl xml/$p.xml > $l/$p.html
+    done
+    xsltproc --stringparam date "`date "+%Y-%m-%dT%H:%M:%SZ"`" --stringparam lang $l xml/geexbox.xsl xml/index.xml > $l/news.xml
+
+  fi
 done
