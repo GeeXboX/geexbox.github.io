3b86bdaff0866acba562e2dfafcb899d988f2496
Alexis Saettler <asbin@asbin.org>
2006-08-24 22:13:16 +0000

    add parameter to choose the backend to use

--- gen.sh
+++ gen.sh
@@ -1,23 +1,42 @@
 #!/bin/sh
+# Generate html pages.
+#  usage : ./gen.sh [param]
+#    param : backend to use : sabcmd or xsltproc
+#
+
+if [ -z "$1" ]; then
+  if [ -n "`which sabcmd`" ]; then
+    BACKEND=sabcmd
+  else
+    BACKEND=xsltproc
+  fi
+else
+  BACKEND=$1
+fi
+
 
 PAGES="index old start license screenshot bugs requirements downloads buy doc controls generator hard faq links sponsors press mailing contact dev donate packages 404"
 LANGS="en fr de"
 
 for l in $LANGS; do
   mkdir -p $l
-  if [ -n "`which sabcmd`" ]; then
+
+  case $BACKEND in
+  sabcmd)
 
     for p in $PAGES; do
       sabcmd --batch-xsl xml/geexbox.xsl xml/$p.xml $l/$p.html \$filename=$p.html \$lang=$l
     done
     sabcmd --batch-xsl xml/atom.xsl xml/index.xml $l/news.xml \$date=`date "+%Y-%m-%dT%H:%M:%SZ"` \$lang=$l
-
-  else
+    ;;
+  
+  xsltproc)
 
     for p in $PAGES; do
       xsltproc --stringparam filename $p.html --stringparam lang $l xml/geexbox.xsl xml/$p.xml > $l/$p.html
     done
     xsltproc --stringparam date "`date "+%Y-%m-%dT%H:%M:%SZ"`" --stringparam lang $l xml/geexbox.xsl xml/index.xml > $l/news.xml
 
-  fi
+    ;;
+  esac
 done
